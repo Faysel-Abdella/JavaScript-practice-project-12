2c457570506930b1bd4693e3431b184900e2af5d
DESKTOP-GHRRVG9\pc <fayselcode@gmail.com>
2023-05-23 17:14:59 +0100

    Random words generated to be displayed in DOM. The score bar increament feature is added.

---
+++ script.js
@@ -0,0 +1,71 @@
+const word = document.getElementById("word");
+const text = document.getElementById("text");
+const scoreEl = document.getElementById("score");
+const timeEl = document.getElementById("time");
+const endgameEl = document.getElementById("end-game-container");
+const settingsBtn = document.getElementById("settings-btn");
+const settings = document.getElementById("settings");
+const settingsForm = document.getElementById("settings-form");
+const difficultySelect = document.getElementById("difficulty");
+
+let randomWord; // To store one random word
+let score = 0; // Initially zero
+let time = 10; // Initially 10 sec
+
+// List of words for game
+const words = [
+  "sigh",
+  "tense",
+  "airplane",
+  "ball",
+  "pies",
+  "juice",
+  "warlike",
+  "bad",
+  "north",
+  "dependent",
+  "steer",
+  "silver",
+  "highfalutin",
+  "superficial",
+  "quince",
+  "eight",
+  "feeble",
+  "admit",
+  "drag",
+  "loving",
+];
+
+// Generate random word from array
+function getRandomWord() {
+  return words[Math.floor(Math.random() * words.length)];
+}
+
+// Add word to DOM
+function addWordToDOM() {
+  randomWord = getRandomWord();
+  word.innerHTML = randomWord;
+}
+
+function updateScore() {
+  score++;
+  scoreEl.innerHTML = score;
+}
+
+addWordToDOM();
+
+// Event listener
+
+text.addEventListener("input", (e) => {
+  const insertdText = e.target.value;
+
+  if (insertdText === randomWord) {
+    setTimeout(() => {
+      addWordToDOM();
+      updateScore();
+
+      // Cleare the input
+      e.target.value = "";
+    }, 300);
+  }
+});
